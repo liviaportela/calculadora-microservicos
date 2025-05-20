b6fd76364f64b7a01d9f6a6f6a7e1a1a956b1c03
Lívia <liviaportelaferreira@gmail.com>
2025-05-20 16:02:01 -0300

    corrigindo CORS origin

--- api/calculate.js
+++ api/calculate.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const serviceUrls = require('../config/serviceUrls');
 
 module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://calculadora-microservicos-zeta.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
