29513530f058d8b0b71c3e81e6bdd509c3739308
Lívia <liviaportelaferreira@gmail.com>
2025-05-20 16:14:43 -0300

    corrigindo app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const calculate = require('../../api/calculate');
-const serviceUrls = require('../../config/serviceUrls');
+const calculate = require('../api/calculate');
+const serviceUrls = require('../config/serviceUrls');
 
 const app = express();
 
