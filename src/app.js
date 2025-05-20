722be1c89fbb305803155eb8a4275fd545013026
Lívia <liviaportelaferreira@gmail.com>
2025-05-20 16:13:39 -0300

    corrigindo app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const calculate = require('../api/calculate');
-const serviceUrls = require('../config/serviceUrls');
+const calculate = require('../../api/calculate');
+const serviceUrls = require('../../config/serviceUrls');
 
 const app = express();
 
@@ -11,6 +11,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/calculate', calculate);
 
 app.get('/api/config', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://calculadora-microservicos-zeta.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') return res.status(200).end();
     res.json(serviceUrls);
 });
 
@@ -18,7 +22,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
